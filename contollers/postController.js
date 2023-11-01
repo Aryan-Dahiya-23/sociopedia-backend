233f5fa36d8349287594419c801e2b25e2d9b6dc
Aryan Dahiya <114468859+Aryan-Dahiya-23@users.noreply.github.com>
2023-11-01 23:10:47 +0530

    Update postController.js
    fetch other's posts only

--- contollers/postController.js
+++ contollers/postController.js
@@ -32,8 +32,8 @@ export const createPost = async (req, res) => {
 export const fetchPosts = async (req, res) => {
   const { id } = req.query;
   try {
-    // const posts = await Post.find({ "createdBy.id": { $ne: id } });
-    const posts = await Post.find();
+    const posts = await Post.find({ "createdBy.id": { $ne: id } });
+    // const posts = await Post.find();
     posts.sort(() => Math.random() - 0.5);
     res.status(200).json(posts);
   } catch (error) {
